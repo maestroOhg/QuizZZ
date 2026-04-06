95dc05a6a86fc0eee90e49bc83107bb6a2cf4204
maestroOhg <pasabumastro@gmail.com>
2026-04-06 22:57:52 +0300

    Небольшая правка

--- app/src/main/java/com/example/quizzz/MainActivity.java
+++ app/src/main/java/com/example/quizzz/MainActivity.java
@@ -1,23 +1,18 @@
 package com.example.quizzz;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
     ListView listView;
     private TopicAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,29 +24,16 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         listView = findViewById(R.id.listView);
-
-        // 2. Получаем данные из репозитория
         List<Topic> topics = TopicRepository.getInstance().getTopics();
-
-        // 3. Создаём адаптер и передаём в него контекст и список
         adapter = new TopicAdapter(this, topics);
-
-        // 4. Привязываем адаптер к списку
         listView.setAdapter(adapter);
-
-        // 5. Обрабатываем клик по элементу списка
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Получаем выбранную тему
                 Topic selectedTopic = topics.get(position);
-
-                // Переход к экрану квиза
                 Intent intent = new Intent(MainActivity.this, QuizActivity.class);
                 intent.putExtra("TOPIC_TITLE", selectedTopic.getTitle());
-                // Передаём список вопросов (требуется, чтобы Question реализовывал Serializable)
                 intent.putExtra("QUESTIONS_LIST", new java.util.ArrayList<>(selectedTopic.getQuestions()));
-
                 startActivity(intent);
             }
             });
--- app/src/main/java/com/example/quizzz/QuizActivity.java
+++ app/src/main/java/com/example/quizzz/QuizActivity.java
@@ -1,4 +1,58 @@
 package com.example.quizzz;
 
-public class QuizActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizActivity extends AppCompatActivity {
+    ListView listView;
+    private TopicAdapter adapter;
+    private String titleQuestion;
+    private List<Question> questions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.question_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.quiz), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        this.titleQuestion = getIntent().getStringExtra("TOPIC_TITLE");
+        this.titleQuestion = (this.titleQuestion == null) ? "Заголовка нет" : this.titleQuestion;
+        {
+            if ((List<Question>) getIntent().getSerializableExtra("QUESTIONS_LIST") == null) this.questions = new ArrayList<Question>();
+            else{
+                this.questions = (List<Question>) getIntent().getSerializableExtra("QUESTIONS_LIST");
+        }
+//        // 5. Обрабатываем клик по элементу списка
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                // Получаем выбранную тему
+//                Topic selectedTopic = topics.get(position);
+//
+//                // Переход к экрану квиза
+//                Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+//                intent.putExtra("TOPIC_TITLE", selectedTopic.getTitle());
+//                // Передаём список вопросов (требуется, чтобы Question реализовывал Serializable)
+//                intent.putExtra("QUESTIONS_LIST", new java.util.ArrayList<>(selectedTopic.getQuestions()));
+//                startActivity(intent);
+//            }
+//        });
+    }
 }
